3e91c4c2e2979926e84f22ef37aee0f6963c8036
Mehdi Eidi <mehdiadq@gmail.com>
2022-04-01 23:00:00 +0430

    rwmutex replaced mutex

--- set/set.go
+++ set/set.go
@@ -5,13 +5,13 @@ import "sync"
 
 // Set contains a map as its backing data structure and a mutex to ensure thread safety.
 type Set struct {
-	Lock sync.Mutex
+	Lock sync.RWMutex
 	Data map[string]struct{}
 }
 
 // New initializes and returns a new set.
 func New() *Set {
-	return &Set{sync.Mutex{}, make(map[string]struct{})}
+	return &Set{sync.RWMutex{}, make(map[string]struct{})}
 }
 
 // Add just adds a new string to the set.
@@ -23,8 +23,8 @@ func (s *Set) Add(str string) {
 
 // Has returns true if the set has str.
 func (s *Set) Has(str string) bool {
-	s.Lock.Lock()
-	defer s.Lock.Unlock()
+	s.Lock.RLock()
+	defer s.Lock.RUnlock()
 	_, ok := s.Data[str]
 	return ok
 }
