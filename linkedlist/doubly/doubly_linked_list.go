26e78a497a5879bc39dec09f39601df947314ace
Mehdi Eidi <mehdiadq@gmail.com>
2022-04-13 21:36:46 +0430

    clone toSlice stuff added for doubly

--- linkedlist/doubly/doubly_linked_list.go
+++ linkedlist/doubly/doubly_linked_list.go
@@ -3,29 +3,31 @@ package doubly
 import "fmt"
 
 type DoublyLinkedList[T comparable] struct {
-	Header  *Node[T]
-	Trailer *Node[T]
+	header  *Node[T]
+	trailer *Node[T]
 	Size    int
 }
 
 func New[T comparable]() *DoublyLinkedList[T] {
 	var d DoublyLinkedList[T]
 
-	d.Header = &Node[T]{}
-	d.Trailer = &Node[T]{Prev: d.Header}
-	d.Header.Next = d.Trailer
+	d.header = &Node[T]{}
+	d.trailer = &Node[T]{Prev: d.header}
+	d.header.Next = d.trailer
 
 	return &d
 }
 
-func (d *DoublyLinkedList[T]) IsEmpty() bool { return d.Size == 0 }
+func (d *DoublyLinkedList[T]) IsEmpty() bool {
+	return d.Size == 0
+}
 
 func (d *DoublyLinkedList[T]) First() (data T, ok bool) {
 	if d.IsEmpty() {
 		return
 	}
 
-	return d.Header.Next.Data, true
+	return d.header.Next.Data, true
 }
 
 func (d *DoublyLinkedList[T]) Last() (data T, ok bool) {
@@ -33,7 +35,7 @@ func (d *DoublyLinkedList[T]) Last() (data T, ok bool) {
 		return
 	}
 
-	return d.Trailer.Prev.Data, true
+	return d.trailer.Prev.Data, true
 }
 
 func (d *DoublyLinkedList[T]) AddBetween(data T, predecessor *Node[T], successor *Node[T]) {
@@ -46,11 +48,11 @@ func (d *DoublyLinkedList[T]) AddBetween(data T, predecessor *Node[T], successor
 }
 
 func (d *DoublyLinkedList[T]) AddFirst(data T) {
-	d.AddBetween(data, d.Header, d.Header.Next)
+	d.AddBetween(data, d.header, d.header.Next)
 }
 
 func (d *DoublyLinkedList[T]) AddLast(data T) {
-	d.AddBetween(data, d.Trailer.Prev, d.Trailer)
+	d.AddBetween(data, d.trailer.Prev, d.trailer)
 }
 
 func (d *DoublyLinkedList[T]) Remove(node *Node[T]) T {
@@ -70,7 +72,7 @@ func (d *DoublyLinkedList[T]) RemoveFirst() (data T, ok bool) {
 		return
 	}
 
-	return d.Remove(d.Header.Next), true
+	return d.Remove(d.header.Next), true
 }
 
 func (d *DoublyLinkedList[T]) RemoveLast() (data T, ok bool) {
@@ -78,19 +80,78 @@ func (d *DoublyLinkedList[T]) RemoveLast() (data T, ok bool) {
 		return
 	}
 
-	return d.Remove(d.Trailer.Prev), true
+	return d.Remove(d.trailer.Prev), true
 }
 
 func (d *DoublyLinkedList[T]) String() string {
 	str := "[ "
 
-	current := d.Header.Next
-
-	for ; current != d.Trailer; current = current.Next {
+	for current := d.header.Next; current != d.trailer; current = current.Next {
 		str += fmt.Sprint(current.Data) + " "
 	}
 
 	str += "]"
 
 	return str
+}
+
+func (d *DoublyLinkedList[T]) Equals(other *DoublyLinkedList[T]) bool {
+	if other == nil {
+		return false
+	}
+
+	current1 := d.header.Next
+	current2 := other.header.Next
+
+	for current1 != nil {
+		if !current1.Equals(current2) {
+			return false
+		}
+
+		current1 = current1.Next
+		current2 = current2.Next
+	}
+
+	return true
+}
+
+func (d *DoublyLinkedList[T]) Clone() *DoublyLinkedList[T] {
+	newDoubly := New[T]()
+
+	if d.Size == 0 {
+		return newDoubly
+	}
+
+	newDoubly.header.Next = &Node[T]{Data: d.header.Next.Data}
+	newDoubly.Size++
+
+	newDoublyTail := newDoubly.header.Next
+
+	walk := d.header.Next.Next
+	var n *Node[T]
+
+	for walk != d.trailer {
+		n = &Node[T]{Data: walk.Data, Prev: newDoublyTail}
+
+		newDoublyTail.Next = n
+		newDoubly.Size++
+
+		newDoublyTail = n
+		walk = walk.Next
+	}
+
+	n.Next = newDoubly.trailer
+	newDoubly.trailer.Prev = n
+
+	return newDoubly
+}
+
+func (d *DoublyLinkedList[T]) ToSlice() []T {
+	r := make([]T, d.Size)
+
+	for i, cur := 0, d.header.Next; cur != d.trailer && i < len(r); i, cur = i+1, cur.Next {
+		r[i] = cur.Data
+	}
+
+	return r
 }
--- linkedlist/doubly/node.go
+++ linkedlist/doubly/node.go
@@ -5,3 +5,10 @@ type Node[T comparable] struct {
 	Prev *Node[T]
 	Next *Node[T]
 }
+
+func (n *Node[T]) Equals(other *Node[T]) bool {
+	if other == nil {
+		return false
+	}
+	return n.Data == other.Data
+}
