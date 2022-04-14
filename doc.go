92c55ef9394fca51b708d6b3d6279fbdbe9ae554
Mehdi Eidi <mehdiadq@gmail.com>
2022-04-14 21:01:25 +0430

    some refactoring on gods package and doc

---
+++ doc.go
@@ -0,0 +1,11 @@
+// Package gods contains some convenience functions for creating and returning new data structures.
+
+/*
+	Supported data structure:
+		* bitset
+		* singly linked list
+		* doubly linked list
+		* circularly linked list
+		* set
+*/
+package gods
--- gods.go
+++ gods.go
@@ -7,24 +7,18 @@ import (
 	"github.com/MehdiEidi/gods/set"
 )
 
-type linked[T comparable] struct{}
-
-func (l linked[T]) Singly() *singly.SinglyLinkedList[T] {
+func NewSinglyLinkedList[T comparable]() *singly.SinglyLinkedList[T] {
 	return singly.New[T]()
 }
 
-func (l linked[T]) Doubly() *doubly.DoublyLinkedList[T] {
+func NewDoublyLinkedList[T comparable]() *doubly.DoublyLinkedList[T] {
 	return doubly.New[T]()
 }
 
-func (l linked[T]) Circularly() *circularly.CircularlyLinkedList[T] {
+func NewCircularlyLinkedList[T comparable]() *circularly.CircularlyLinkedList[T] {
 	return circularly.New[T]()
 }
 
-func Linked[T comparable]() linked[T] {
-	return linked[T]{}
-}
-
-func Set[T comparable]() *set.Set[T] {
+func NewSet[T comparable]() *set.Set[T] {
 	return set.New[T]()
 }
