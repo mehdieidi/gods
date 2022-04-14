8b2503fc2974628e9f8fd902455e069de4ce5f13
amirkhaki <95536619+amirkhaki@users.noreply.github.com>
2022-04-14 07:47:14 +0430

    Add some helper functions

---
+++ gods.go
@@ -0,0 +1,25 @@
+package gods 
+
+import (
+    "github.com/MehdiEidi/gods/linkedlist/singly"
+    "github.com/MehdiEidi/gods/linkedlist/doubly"
+    "github.com/MehdiEidi/gods/set"
+)
+
+type linked[T comparable] struct{}
+
+func (l linked[T]) Singly() *singly.SinglyLinkedList[T] {
+    return singly.New[T]()
+}
+
+func (l linked[T]) Doubly() *doubly.DoublyLinkedList[T] {
+    return doubly.New[T]()
+}
+
+func Linked[T comparable]() linked[T] {
+    return linked[T]{}
+}
+
+func Set[T comparable]() *set.Set[T] {
+    return set.New[T]()
+}
