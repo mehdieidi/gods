27ecf50ee6297bd44b794281fea7ba22afde1e9e
Mehdi Eidi <mehdiadq@gmail.com>
2022-04-21 18:40:08 +0430

    general tree extended

--- tree/general/interface.go
+++ tree/general/interface.go
@@ -2,14 +2,26 @@ package tree
 
 type Tree[T comparable] interface {
 	Root() *Node[T]
+	SetRoot(*Node[T])
+	Size() int
+	SetSize(int)
 	Parent(*Node[T]) *Node[T]
 	Children(*Node[T]) []*Node[T]
 	ChildrenNum(*Node[T]) int
 	IsInternal(*Node[T]) bool
 	IsExternal(*Node[T]) bool
 	IsRoot(*Node[T]) bool
-	Size() int
 	IsEmpty() bool
 	Height(*Node[T]) int
 	Depth(*Node[T]) int
+	AddRoot(T) (*Node[T], error)
+	AddChildTo(*Node[T], T) *Node[T]
+	Set(*Node[T], T) T
+	String() string
+	PreOrder() []*Node[T]
+	PostOrder() []*Node[T]
+	BFS() []*Node[T]
+	DFS() []*Node[T]
+	EulerTour() []*Node[T]
+	Parenthesize(*Node[T])
 }
--- tree/general/tree.go
+++ tree/general/tree.go
@@ -1,6 +1,17 @@
 package tree
 
-import "math"
+import (
+	"errors"
+	"fmt"
+	"math"
+
+	"github.com/MehdiEidi/gods/queue"
+	"github.com/MehdiEidi/gods/stack"
+)
+
+var (
+	TreeNotEmptyErr = errors.New("tree is not empty")
+)
 
 type generalTree[T comparable] struct {
 	root *Node[T]
@@ -13,6 +24,10 @@ func New[T comparable]() Tree[T] {
 
 func (gt *generalTree[T]) Root() *Node[T] {
 	return gt.root
+}
+
+func (gt *generalTree[T]) SetRoot(n *Node[T]) {
+	gt.root = n
 }
 
 func (gt *generalTree[T]) Parent(n *Node[T]) *Node[T] {
@@ -43,6 +58,10 @@ func (gt *generalTree[T]) Size() int {
 	return gt.size
 }
 
+func (gt *generalTree[T]) SetSize(s int) {
+	gt.size = s
+}
+
 func (gt *generalTree[T]) IsEmpty() bool {
 	return gt.size == 0
 }
@@ -60,4 +79,157 @@ func (gt *generalTree[T]) Height(n *Node[T]) (h int) {
 		h = int(math.Max(float64(h), 1.0+float64(gt.Height(c))))
 	}
 	return
+}
+
+func (gt *generalTree[T]) AddRoot(data T) (*Node[T], error) {
+	if !gt.IsEmpty() {
+		return nil, TreeNotEmptyErr
+	}
+
+	gt.root = &Node[T]{Data: data}
+	gt.size++
+
+	return gt.root, nil
+}
+
+func (gt *generalTree[T]) AddChildTo(parent *Node[T], data T) *Node[T] {
+	child := &Node[T]{Data: data, Parent: parent}
+	parent.Children = append(parent.Children, child)
+
+	gt.size++
+
+	return child
+}
+
+func (gt *generalTree[T]) Set(n *Node[T], data T) (val T) {
+	val = n.Data
+	n.Data = data
+	return
+}
+
+func (gt *generalTree[T]) PreOrder() (list []*Node[T]) {
+	if !gt.IsEmpty() {
+		gt.preOrderUtil(gt.root, &list)
+	}
+	return
+}
+
+func (gt *generalTree[T]) preOrderUtil(n *Node[T], list *[]*Node[T]) {
+	*list = append(*list, n)
+
+	for _, c := range gt.Children(n) {
+		gt.preOrderUtil(c, list)
+	}
+}
+
+func (gt *generalTree[T]) String() string {
+	list := gt.PreOrder()
+
+	str := "[ "
+
+	for _, n := range list {
+		str += fmt.Sprint(n.Data) + " "
+	}
+
+	str += "]"
+
+	return str
+}
+
+func (gt *generalTree[T]) PostOrder() (list []*Node[T]) {
+	if !gt.IsEmpty() {
+		gt.postOrderUtil(gt.root, &list)
+	}
+	return
+}
+
+func (gt *generalTree[T]) postOrderUtil(n *Node[T], list *[]*Node[T]) {
+	for _, c := range gt.Children(n) {
+		gt.postOrderUtil(c, list)
+	}
+
+	*list = append(*list, n)
+}
+
+func (gt *generalTree[T]) BFS() (list []*Node[T]) {
+	if gt.IsEmpty() {
+		return
+	}
+
+	queue := queue.NewLinkedQueue[*Node[T]]()
+	queue.Enqueue(gt.root)
+
+	for !queue.IsEmpty() {
+		n, _ := queue.Dequeue()
+
+		list = append(list, n)
+
+		for _, c := range gt.Children(n) {
+			queue.Enqueue(c)
+		}
+	}
+
+	return
+}
+
+func (gt *generalTree[T]) DFS() (list []*Node[T]) {
+	if gt.IsEmpty() {
+		return
+	}
+
+	stack := stack.NewLinkedStack[*Node[T]]()
+	stack.Push(gt.root)
+
+	for !stack.IsEmpty() {
+		n, _ := stack.Pop()
+
+		list = append(list, n)
+
+		for _, c := range gt.Children(n) {
+			stack.Push(c)
+		}
+	}
+
+	return
+}
+
+func (gt *generalTree[T]) EulerTour() (list []*Node[T]) {
+	if !gt.IsEmpty() {
+		gt.eulerTourUtil(gt.root, &list)
+	}
+	return
+}
+
+func (gt *generalTree[T]) eulerTourUtil(n *Node[T], list *[]*Node[T]) {
+	*list = append(*list, n)
+
+	for _, c := range gt.Children(n) {
+		gt.eulerTourUtil(c, list)
+	}
+
+	*list = append(*list, n)
+}
+
+func (gt *generalTree[T]) Parenthesize(n *Node[T]) {
+	fmt.Print(n.Data)
+
+	if gt.IsExternal(n) {
+		return
+	}
+
+	firstTime := true
+
+	for _, c := range gt.Children(n) {
+		if firstTime {
+			fmt.Print(" (")
+		} else {
+			fmt.Print(", ")
+		}
+
+		firstTime = false
+
+		gt.Parenthesize(c)
+	}
+
+	fmt.Print(")")
 }
